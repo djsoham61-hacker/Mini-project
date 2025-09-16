7ca5a8cc5604a82719f76bcffa2853f77c70d7a6
Soham Salunkhe <djsoham61@gmail.com>
2025-09-16 10:27:06 +0530

    update registration.py

--- registration.py
+++ registration.py
@@ -1,8 +1,36 @@
-user=str(input("Enter your username:"))
-# print(type(user))
-if not user.isalpha():
-    raise ValueError('Enter only A-Z')
-else:
-    print("Registration Successfully")
+import os
+import random as rd
+import json
 
-print(user)
+
+def registration():
+    user=str(input("Enter your username:"))
+    us_f=f'{user}.json'
+    if os.path.exists(us_f):
+        print('User already signed in')
+        print('Go to login')
+        login()
+    else:
+        print('Register Sucessfully')
+        id=rd.randint(1,1001)
+        print(f'your id is:{id}')
+        data={'Username':us_f,'Id':id}
+        # print(data)
+        with open(us_f,'w') as fp:
+            return json.dump(data,fp)
+# registration() 
+
+def login():
+    user=str(input("Enter your username:"))
+    file=f'{user}.json'
+    id_ent=int(input('Enter id:'))
+    if not os.path.exists(file):
+        print('User not registered:')
+        registration()
+    else:
+        with open(file,'r') as fp:
+            data = json.load(fp)
+        if (data['Id'] == id_ent and data['Username'] == file):
+
+            print('Login Sucessfully')
+login()
